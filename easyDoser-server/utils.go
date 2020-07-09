f2b3845ad50b366f91064af3edcb3f3261e0c2a2
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2020-07-09 17:05:59 +0530

    Server now returns collection config

--- easyDoser-server/utils.go
+++ easyDoser-server/utils.go
@@ -45,6 +45,17 @@ func getArgsForCCList(r *http.Request) [6]string {
 	theArray := [6]string{cfg, address, mspId, mspConfig, tls, channel}
 	return theArray
 }
+func getArgsForCConfig(r *http.Request) [7]string {
+	cfg := r.FormValue("cfg")
+	address := r.FormValue("peer-address")
+	mspId := r.FormValue("msp-id")
+	mspConfig := r.FormValue("msp-config")
+	tls := r.FormValue("tls-cert")
+	channel := r.FormValue("channel")
+	cc := r.FormValue("chaincode")
+	theArray := [7]string{cfg, address, mspId, mspConfig, tls, channel, cc}
+	return theArray
+}
 
 //http://localhost:8080/channel_info/mychannel?cfg=%22/mnt/265C6B275C6AF14B/fabric/config%22&orderer-address=%22localhost:7050%22&msp-id=%22Org1MSP%22&msp-config=%22/mnt/265C6B275C6AF14B/fabric/test-network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp%22&orderer-certificate=%22/mnt/265C6B275C6AF14B/fabric/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem%22
 // http://localhost:8080/channel_list?cfg=%22/mnt/265C6B275C6AF14B/fabric/config%22&peer-address=%22localhost:7051%22&msp-id=%22Org1MSP%22&msp-config=%22/mnt/265C6B275C6AF14B/fabric/test-network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp%22&tls-cert=%22/mnt/265C6B275C6AF14B/fabric/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt%22
