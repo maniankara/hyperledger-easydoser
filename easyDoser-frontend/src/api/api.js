6194fc6d7fd6651b45a04407e3f43f45d1f61bae
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2020-07-31 17:45:58 +0530

    done with checking commit readiness

--- easyDoser-frontend/src/api/api.js
+++ easyDoser-frontend/src/api/api.js
@@ -102,5 +102,38 @@ export const approveConfig = async (policy, aPolicy, version, channel, chaincode
   return await resp.json()
 
 }
+export const checkCommit = async (policy, aPolicy, version, channel, chaincode) => {
+  const cookies = new Cookies();
+  const tcfg = cookies.get(cfg)
+  const tmspid = cookies.get(mspid)
+  const tpa = cookies.get(pa)
+  const tmspconf = cookies.get(mspconf)
+  const ttls = cookies.get(tls)
+  const toc = cookies.get(oc)
+  const toa = cookies.get(oa)
+
+  var opts = {
+    policy : policy, 
+    aPolicy : aPolicy,
+    version : version,
+    cfg : tcfg,
+    mspid : tmspid,
+    pa : tpa,
+    mspconf : tmspconf,
+    tls : ttls,
+    oa : toa,
+    oc : toc,
+    channel: channel,
+    chaincode : chaincode
+
+  }
+  const tserver = cookies.get(server)
+  var resp = await fetch ("http://"+tserver+"/check",{
+    method: 'post',
+    body: JSON.stringify(opts)
+  });
+  return await resp.json()
+
+}
 //      "?cfg=/mnt/265C6B275C6AF14B/fabric/config&orderer-address=localhost:7050&msp-id=Org1MSP&msp-config=/mnt/265C6B275C6AF14B/fabric/test-network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp&orderer-certificate=/mnt/265C6B275C6AF14B/fabric/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
 //    "http://localhost:8080/channel_list?cfg=/mnt/265C6B275C6AF14B/fabric/config&peer-address=localhost:7051&msp-id=Org1MSP&msp-config=/mnt/265C6B275C6AF14B/fabric/test-network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp&tls-cert=/mnt/265C6B275C6AF14B/fabric/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt"
