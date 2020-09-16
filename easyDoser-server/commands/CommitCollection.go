8ff6c71f15399c00500420b9005f4e633f24fdad
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2020-09-16 12:45:45 +0530

    fixed pushing error as status
    Signed-off-by: Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>

--- easyDoser-server/commands/CommitCollection.go
+++ easyDoser-server/commands/CommitCollection.go
@@ -75,7 +75,7 @@ func CommitChaincode(w http.ResponseWriter, r *http.Request) {
 		}
 		str := reg.ReplaceAllString(stderr.String(), " ")
 		fmt.Println(fmt.Sprint(err) + ": " + "{\"error\":\"" + str + "\"}")
-		fmt.Fprintf(w, "{\"error\":\""+str+"\"}")
+		fmt.Fprintf(w, "{\"status\":\""+str+"\"}")
 		return
 	}
 	fmt.Println(out.String())
